da5f8c3f8ef9d5f9815d43ecf714f28a1aac4360
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-04 11:48:17 +0100

    summed up playGame()
    Fixed minor issues with functionality of the playGame function by moving some playRound() and winStatus() into it to make them local. Also adjusted the getHumanChoice() so that it locally alerts for a wrong input.

--- rps.js
+++ rps.js
@@ -1,9 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
-const human = getHumanChoice();
-const computer = getComputerChoice(); 
-
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     if (num === 0){
@@ -17,40 +11,67 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let choice = prompt("Enter 'rock' 'paper' or 'scissors'");
+    while (choice != "rock" && choice != 'paper' && choice != 'scissors'){
+        alert("Invalid Entry TRY AGAIN!");
+        choice = prompt("Enter 'rock' 'paper' or 'scissors'");
+    }
     return choice.toLowerCase();
 }
 
-function winStatus(human, computer){
-    switch (`${human}_${computer}`) {
-        case 'rock_scissors':
-        case 'paper_rock':
-        case 'scissors_paper':
-            humanScore++;
-            return "win"
-        case 'rock_rock':
-        case 'paper_paper':
-        case 'scissors_scissors':
-            return "draw"
-        default:
-            computerScore++;
-            return "lose";
+
+
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    let human = getHumanChoice();
+    let computer = getComputerChoice();
+
+    //playRound Function
+    function playRound(human, computer){
+
+        console.log(
+        `
+        Your Choice: ${human}
+        Computer Choice: ${computer}
+        You ${winStatus(human, computer)} this round
+        YOU - COMPUTER
+        ${humanScore} - ${computerScore}`)
+    }
+
+    //WinStatus Function
+    function winStatus(human, computer){
+        switch (`${human}_${computer}`) {
+            case 'rock_scissors':
+            case 'paper_rock':
+            case 'scissors_paper':
+                humanScore++;
+                return "win"
+            case 'rock_rock':
+            case 'paper_paper':
+            case 'scissors_scissors':
+                return "draw"
+            default:
+                computerScore++;
+                return "lose";
+        }
+    }
+    
+
+    for(let i = 0; i < 5; i++){
+        human = getHumanChoice();
+        computer = getComputerChoice();
+        playRound(human, computer);
+    }
+
+    if (humanScore > computerScore){
+        console.log("YOU WIN!!!!")
+    } else if (humanScore < computerScore){
+        console.log("YOU LOSE!!!")
+    } else{
+        console.log("YOU DRAW :)")
     }
 }
 
-function playRound(human, computer){
-    if (human != "rock" && human != 'paper' && human != 'scissors'){
-        alert("Invalid Entry TRY AGAIN!")
-        playRound();
-        return
-    }
-
-    console.log(
-    `
-    Your Choice: ${human}
-    Computer Choice: ${computer}
-    You ${winStatus(human, computer)} this round
-    YOU - COMPUTER
-    ${humanScore} - ${computerScore}`)
-}
-
-playRound()
+playGame()
