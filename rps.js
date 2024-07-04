dc6197c546dd4357578d5965ecca1fa97579bf86
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-04 10:06:03 +0100

    Write get computer value function
    Using random I added a function to get a random play from the computer between rock paper and scissors.

---
+++ rps.js
@@ -0,0 +1,11 @@
+function getComputerChoice(){
+    let num = Math.floor(Math.random() * 3);
+    if (num === 0){
+        return "rock"
+    } else if (num === 1){
+        return "paper"
+    } else if (num === 2){
+        return "scissors"
+    }
+}
+
