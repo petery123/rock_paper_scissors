fa445efbf809438a68e3d20294e378d39e57e22e
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-04 10:29:55 +0100

    Write fuction to play a round
    The fuction collects the plays of the computer and the user and compares them to see who won the round and prints the out put.

--- rps.js
+++ rps.js
@@ -13,5 +13,32 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    return prompt("Enter 'rock' 'paper' or 'scissors'");
+    let choice = prompt("Enter 'rock' 'paper' or 'scissors'");
 }
+
+function playRound(){
+    let human = getHumanChoice();
+    let computer = getComputerChoice();
+    let status = "win";
+
+    switch (`${human}_${computer}`) {
+        case 'rock_scissors':
+        case 'paper_rock':
+        case 'scissors_paper':
+            humanScore ++;
+            break;
+        default:
+            computerScore ++;
+            status = "lose";
+            break;
+    }
+
+    console.log(
+    `Your Choice: ${human}
+    Computer Choice: ${computer}
+    You ${status} this round
+    YOU - COMPUTER
+    ${humanScore} - ${computerScore}`)
+}
+
+playRound()
