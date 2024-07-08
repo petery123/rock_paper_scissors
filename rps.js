5c6e4e0b830c1b6ca803685f8c91364d45dcff57
petery123 <139976625+petery123@users.noreply.github.com>
2024-07-08 22:51:01 +0100

    Make the game to stop when a player reaches 5
    I updated the code to allow for the game to stop and take in no more input when a player reaches 5. Had issues implementing removeEventHandler

--- rps.js
+++ rps.js
@@ -5,16 +5,28 @@ const paperBtn = document.querySelector("#paperBtn");
 const scissorsBtn = document.querySelector("#scissorsBtn");
 
 const roundWinner = document.querySelector("#roundWinner");
+const overallWinner = document.querySelector("#overallWinner");
+const userBoard = document.querySelector("#userBoard");
+const computerBoard = document.querySelector("#computerBoard");
+const userPlay = document.querySelector("#userPlay");
+const computerPlay = document.querySelector("#computerPlay");
 
-buttons.addEventListener("click", (event) => {
-    let target = event.target.id;
-    //remove 'Btn' from behind target name and runs play game function.
-    playRound(target.slice(0, -3));
-    
-})
-
-let humanScore = 0;
+let userScore = 0;
 let computerScore = 0;
+let play = true;
+
+userBoard.textContent = userScore;
+computerBoard.textContent = computerScore;
+
+buttons.addEventListener("click", (event) => initiate(event));
+
+function initiate(event){
+    if (play){
+        let target = event.target.id;
+        //remove 'Btn' from behind target name and runs play game function.
+        playRound(target.slice(0, -3));
+    }
+}
 
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
@@ -27,11 +39,22 @@ function getComputerChoice(){
     }
 }
 
-function playRound(playerChoice){
+function playRound(userChoice){
     let computerChoice = getComputerChoice();
-    let status = winStatus(playerChoice, computerChoice);
+    let status = winStatus(userChoice, computerChoice);
+    console.log(`User: ${userChoice} Computer: ${computerChoice}`)
 
+    computerPlay.textContent = computerChoice;
+    userPlay.textContent = userChoice
+    userBoard.textContent = userScore;
+    computerBoard.textContent = computerScore;
     roundWinner.textContent = `You ${status} this round`
+
+    if (userScore === 5 || computerScore === 5){
+        play = false;
+        buttons.removeEventListener("click", ((event) => initiate(event)));
+        overallWinner.textContent = userScore > computerScore? "You WIN!" : "Computer WINS!";
+    }
 }
 
 function winStatus(human, computer){
@@ -39,7 +62,7 @@ function winStatus(human, computer){
         case 'rock_scissors':
         case 'paper_rock':
         case 'scissors_paper':
-            humanScore++;
+            userScore++;
             return "win"
         case 'rock_rock':
         case 'paper_paper':
